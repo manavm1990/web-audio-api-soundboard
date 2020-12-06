11f2ffb50c3e83e3a02e2d3e696de43739887764
Manav Misra <manavm@visionwebsoft.com>
2020-12-06 12:21:19 -0600

    :sparkles: Play kick drum

--- sounds/kick.js
+++ sounds/kick.js
@@ -1,11 +1,24 @@
 import { context, volumeControl } from "./lib";
 
 export default () => {
+  const gainControl = context.createGain();
   const oscillator = context.createOscillator();
 
-  // 'C' in Hz. w/o delay
+  gainControl.gain.setValueAtTime(1, 0);
+  gainControl.gain.exponentialRampToValueAtTime(
+    0.001,
+    context.currentTime + 0.5
+  );
+  gainControl.connect(volumeControl);
+
   oscillator.frequency.setValueAtTime(261.6, 0);
-  oscillator.connect(volumeControl);
+  oscillator.frequency.setValueAtTime(150, context.currentTime);
+  oscillator.frequency.exponentialRampToValueAtTime(
+    0.001,
+    context.currentTime + 0.5
+  );
+
+  oscillator.connect(gainControl);
   oscillator.start();
   oscillator.stop(context.currentTime + 0.5);
 };
