bc8636fcca9b17f8f9d9aba559121fcdc6a18c0a
Manav Misra <manavm@visionwebsoft.com>
2020-12-07 04:51:25 -0600

    :sparkles: Play snare drum

--- sounds/lib.js
+++ sounds/lib.js
@@ -1,8 +1,27 @@
 const audioContext = new AudioContext();
 const primaryGainControl = audioContext.createGain();
+const SAMPLE_RATE = audioContext.sampleRate;
+
+// mono (one channel) buffer that holds 1 second worth of audio data.
+const buffer = audioContext.createBuffer(
+  1,
+  SAMPLE_RATE * 1, // 1 sec
+  SAMPLE_RATE
+);
+
+const channelData = buffer.getChannelData(0);
+
+// Fill buffer channel data with random integers (-1 to 1) to create 'white noise.'
+// This will be a basis for some of the things like the 'snare'
+for (let i = 0; i < buffer.length; i++) {
+  channelData[i] =
+    // Math.random() * (max - min) + min
+    Math.random() * (1 + 1) - 1;
+}
 
 primaryGainControl.gain.setValueAtTime(0.05, 0);
 primaryGainControl.connect(audioContext.destination);
 
 export { audioContext as context };
+export { buffer as primaryBuffer };
 export { primaryGainControl as volumeControl };
--- sounds/snare.js
+++ sounds/snare.js
@@ -1,3 +1,35 @@
+import { primaryBuffer, context, volumeControl } from "./lib";
+
+const filter = context.createBiquadFilter();
+const oscGain = context.createGain();
+
+filter.type = "highpass";
+filter.frequency.value = 1500;
+filter.connect(volumeControl);
+
 export default () => {
-  console.log("🥁");
+  const gain = context.createGain();
+  const oscillator = context.createOscillator();
+  const source = context.createBufferSource();
+
+  source.buffer = primaryBuffer;
+  source.connect(gain);
+  source.start();
+  source.stop(context.currentTime + 0.2);
+
+  gain.gain.setValueAtTime(1, context.currentTime);
+  gain.gain.exponentialRampToValueAtTime(0.01, context.currentTime + 0.2);
+  gain.connect(filter);
+
+  oscillator.type = "triangle";
+  oscillator.frequency.setValueAtTime(100, context.currentTime);
+  oscGain.gain.setValueAtTime(0.7, context.currentTime);
+  oscGain.gain.exponentialRampToValueAtTime(0.01, context.currentTime + 0.1);
+
+  oscillator.connect(oscGain);
+
+  oscGain.connect(volumeControl);
+
+  oscillator.start();
+  oscillator.stop(context.currentTime + 0.2);
 };
